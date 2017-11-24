4821043b81f9d230adf51abc88c5c2ef372029e3
sexfat <bryant6831@gmail.com>
2017-11-24 17:15:36 +0800

    Add uglify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
 
+//  uglify js
+gulp.task('compress', function() {
+    // 將你的默認的任務代碼放在這
+    return gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('js/bundle/'));
+  });
+
+// js move
 gulp.task('default', function() {
     // 将妳的任務代碼放在這
     return gulp.src('*.html')
     .pipe(gulp.dest('build/'));
+   
   });
 
+  console.log('no error');
 
